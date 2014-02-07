7e895aea09f8771d20b369244a8ae33eab21ed5e
Charles Max Wood <chuck@devchat.tv>
2014-02-07 02:12:47 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nute::Application.config.secret_key_base = '80d51d658aeeb419f7ab98c11619e19a049c4afe59144634f42c827b41da8bc8ee42e1e2b716dd3ae59df65868583afe07b4b89fb7519db28aa0acfa8f7f9115'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nute::Application.config.session_store :cookie_store, key: '_nute_session'
